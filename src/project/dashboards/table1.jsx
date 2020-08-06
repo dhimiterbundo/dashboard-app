a5bfc030de887694966051a3723f43b6113994ad
Dhimiter Bundo <b.dhimiter@yahoo.com>
2020-08-06 14:17:37 +0200

    wip -changes

--- src/project/dashboards/table1.jsx
+++ src/project/dashboards/table1.jsx
@@ -1,15 +1,23 @@
 import React , {Component} from 'react';
 import axios from 'axios';
+import Button from '@material-ui/core/Button';
+
 class Table extends Component {
     // state = {  }
 componentDidMount(){
-    axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
+ this.getData();
+}
+getData(){
+    axios.get('https://coronavirus.al/api/stats.php').then(response => {
         console.log(response)
     })
     .catch(error=>{console.log(error)});
 }
     render() { 
-        return <h1>Hello World</h1>;
+        return <div>  <h1>Hello World</h1>
+        <Button variant="contained" color="primary">
+        Get Data
+      </Button></div>;
     }
 }
 export default Table;
--- src/project/layout/header.jsx
+++ src/project/layout/header.jsx
@@ -1,8 +1,39 @@
 import React , {Component} from 'react';
+// eslint-disable-next-line no-unused-vars
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import HomeIcon from '@material-ui/icons/Home';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import PersonPinIcon from '@material-ui/icons/PersonPin';
+
+
 class Header extends Component {
+     useStyles = makeStyles({
+        root: {
+          flexGrow: 1,
+          maxWidth: 500,
+        },
+      });
+      
     // state = {  }
     render() { 
-        return <h1>Hello World</h1>;
+        return (
+            <Paper square>
+              <Tabs
+                variant="fullWidth"
+                indicatorColor="primary"
+                textColor="primary"
+                aria-label="icon tabs example"
+              >
+                <Tab icon={<HomeIcon />} aria-label="phone" />
+                <Tab icon={<FavoriteIcon />} aria-label="favorite" />
+                <Tab icon={<PersonPinIcon />} aria-label="person" />
+              </Tabs>
+            </Paper>
+          );
+     
     }
 }
 export default Header;
